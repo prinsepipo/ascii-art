a8458f35012d6e25d8b3e1418851942839190a91
pipo <pipo@me.com>
2019-08-10 07:54:06 +0800

    simplified running with less arguments

--- ascii-art.py
+++ ascii-art.py
@@ -4,8 +4,6 @@ from src.converter import render_image
 
 
 if __name__ == "__main__":
-    img_argv = sys.argv[1]
-    if img_argv == '--img':
-        img_path = 'image/' + sys.argv[2]
+    img_path = 'image/' + sys.argv[1]
 
-        render_image(imgpath=img_path)
+    render_image(imgpath=img_path)
