a1a9c73e376f16381693b4f0eae296247b9f843b
Pipo <prinsepipo.sanchez@gmail.com>
2019-10-15 01:16:37 +0800

    Serve staticfiles from s3

--- backend/settings.py
+++ backend/settings.py
@@ -146,6 +146,7 @@ if not DEBUG:
 
     # Media Storage
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # AWS
