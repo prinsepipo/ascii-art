edc92200d0ac1310e99414d11a359d80076378e2
Pipo <prinsepipo.sanchez@gmail.com>
2019-10-16 18:39:45 +0800

    Save new converted image to s3

--- app_converter/signals.py
+++ app_converter/signals.py
@@ -1,18 +1,23 @@
 from django.conf import settings
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.files.storage import default_storage as storage
 
 from .models import AsciiImage, RawImage
 from utils.converter import ImageConverter
 
 from .choices import FILTER_CHOICES
 
+
 import uuid
+import urllib.request
+import io
 
 
 @receiver(post_save, sender=RawImage)
 def convert_image(sender, instance, **kwargs):
-    fp = instance.image.url
+    image_url = instance.image.url
+    fp = io.BytesIO(urllib.request.urlopen(image_url).read())
     mfilter = FILTER_CHOICES[int(instance.filters) - 1][1]
 
     converter = ImageConverter()
@@ -21,7 +26,13 @@ def convert_image(sender, instance, **kwargs):
     image = converter.process_image(fp)
     filename = str(uuid.uuid4()) + '.png'
     new_file_path = settings.MEDIA_ROOT + '/converted/' + filename
-    image.save(new_file_path, format='PNG')
+
+    buffer = io.BytesIO()
+    image.save(buffer, format='PNG')
+    imageFile = storage.open('converted/' + filename, 'wb')
+    imageFile.write(buffer.getvalue())
+    imageFile.flush()
+    imageFile.close()
 
     ascii_image = AsciiImage()
     ascii_image.raw = instance
